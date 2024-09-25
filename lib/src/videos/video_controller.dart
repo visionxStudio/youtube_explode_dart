ab6974315c351405c9882bf9c36cb9998aec2c39
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2024-09-25 13:00:43 +0545

    Update video_controller.dart

--- lib/src/videos/video_controller.dart
+++ lib/src/videos/video_controller.dart
@@ -6,15 +6,12 @@ import '../reverse_engineering/player/player_response.dart';
 
 @internal
 class VideoController {
-  /// Used to fetch streams without signature deciphering, but has limited streams.
-  static const androidTestSuiteClient = {
+  static const _androidSuiteClient = {
     'context': {
       'client': {
-        'clientName': 'ANDROID_CREATOR',
-        'clientVersion': '24.24.100',
+        'clientName': 'ANDROID_TESTSUITE',
+        'clientVersion': '1.9',
         'androidSdkVersion': 30,
-        // 'userAgent':
-        // 'com.google.android.youtube/17.36.4 (Linux; U; Android 12; GB) gzip',
         'hl': 'en',
         'gl': 'US',
         'utcOffsetMinutes': 0,
@@ -22,24 +19,7 @@ class VideoController {
     },
   };
 
-  /// Used to to fetch all the video streams (but has signature deciphering).
-  static const androidClient = {
-    'context': {
-      'client': {
-        'clientName': 'ANDROID',
-        'clientVersion': '19.09.37',
-        'androidSdkVersion': 30,
-        'userAgent':
-            'com.google.android.youtube/19.09.37 (Linux; U; Android 11) gzip',
-        'hl': 'en',
-        'timeZone': 'UTC',
-        'utcOffsetMinutes': 0,
-      },
-    },
-  };
-
-  /// Used to fetch streams for age-restricted videos without authentication.
-  static const tvClient = {
+  static const _tvClient = {
     'context': {
       'client': {
         'clientName': 'TVHTML5_SIMPLY_EMBEDDED_PLAYER',
@@ -63,29 +43,24 @@ class VideoController {
     return WatchPage.get(httpClient, videoId.value);
   }
 
-  Future<PlayerResponse> getPlayerResponse(VideoId videoId,
-      Map<String, Map<String, Map<String, Object>>> client) async {
-    assert(client['context'] != null, 'client must contain a context');
-    assert(client['context']!['client'] != null,
-        'client must contain a context.client');
-
-    final userAgent = client['context']!['client']!['userAgent'] as String?;
-    // From https://github.com/Tyrrrz/YoutubeExplode:
-    // The most optimal client to impersonate is the Android client, because
-    // it doesn't require signature deciphering (for both normal and n-parameter signatures).
-    // However, the regular Android client has a limitation, preventing it from downloading
-    // multiple streams from the same manifest (or the same stream multiple times).
-    // As a workaround, we're using ANDROID_TESTSUITE which appears to offer the same
-    // functionality, but doesn't impose the aforementioned limitation.
-    // https://github.com/Tyrrrz/YoutubeExplode/issues/705
+  Future<PlayerResponse> getPlayerResponse(VideoId videoId) async {
+    /// From https://github.com/Tyrrrz/YoutubeExplode:
+    /// The most optimal client to impersonate is the Android client, because
+    /// it doesn't require signature deciphering (for both normal and n-parameter signatures).
+    /// However, the regular Android client has a limitation, preventing it from downloading
+    /// multiple streams from the same manifest (or the same stream multiple times).
+    /// As a workaround, we're using ANDROID_TESTSUITE which appears to offer the same
+    /// functionality, but doesn't impose the aforementioned limitation.
+    /// https://github.com/Tyrrrz/YoutubeExplode/issues/705
     final content = await httpClient.postString(
       'https://www.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w&prettyPrint=false',
       body: {
-        ...client,
+        ..._androidSuiteClient,
         'videoId': videoId.value,
       },
       headers: {
-        if (userAgent != null) 'User-Agent': userAgent,
+        'User-Agent':
+            'com.google.android.youtube/17.36.4 (Linux; U; Android 12; GB) gzip',
       },
     );
     return PlayerResponse.parse(content);
@@ -101,7 +76,7 @@ class VideoController {
     final content = await httpClient.postString(
       'https://www.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w&prettyPrint=false',
       body: {
-        ...tvClient,
+        ..._tvClient,
         'videoId': videoId.value,
         'playbackContext': {
           'contentPlaybackContext': {
